4f98e00fb6eeae8fc6055f36bd2470f2c31c331c
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2023-11-02 19:59:38 +0100

    Replaced builtin array of MoveDirection for List.

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -2,20 +2,23 @@ package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class OptionsParser {
-    public static MoveDirection[] parse(String[] args){
-        MoveDirection[] commands = new MoveDirection[args.length];
-        int counter = 0;
+    public static List<MoveDirection> parse(String[] args){
+        List<MoveDirection> commands = new ArrayList<>();
+        //MoveDirection[] commands = new MoveDirection[args.length];
+        //int counter = 0;
         for(int i = 0; i != args.length; ++i){
             switch (args[i]){
-                case "f" -> commands[counter++] = MoveDirection.FORWARD;
-                case "b" -> commands[counter++] = MoveDirection.BACKWARD;
-                case "l" -> commands[counter++] = MoveDirection.LEFT;
-                case "r" -> commands[counter++] = MoveDirection.RIGHT;
+                case "f" -> commands.add(MoveDirection.FORWARD);
+                case "b" -> commands.add(MoveDirection.BACKWARD);
+                case "l" -> commands.add(MoveDirection.LEFT);
+                case "r" -> commands.add(MoveDirection.RIGHT);
             }
         }
-        return Arrays.copyOfRange(commands, 0, counter);
+        return commands;
     }
 }
