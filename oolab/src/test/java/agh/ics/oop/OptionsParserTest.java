29e3374736fe4ac71c2a003e3c2c9540422652f6
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2023-12-12 12:01:01 +0100

    Update OptionsParserTest.java

--- oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
+++ oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -12,28 +12,28 @@ public class OptionsParserTest {
     @Test
     public void parseTest() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            String[] args1 = {"f", "b", "l", "r", "forward", "up", "BACKWARD", "b", "r", "r", "l", "f"};
+            List<String> args1 = List.of("f", "b", "l", "r", "forward", "up", "BACKWARD", "b", "r", "r", "l", "f");
             List<MoveDirection> results1 = OptionsParser.parse(args1);
             List<MoveDirection> expected1 = new ArrayList<>(Arrays.asList(MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.RIGHT, MoveDirection.BACKWARD, MoveDirection.RIGHT, MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.FORWARD));
             Assertions.assertEquals(results1, expected1);
         });
 
         Assertions.assertDoesNotThrow(() -> {
-            String[] args2 = {};
+            List<String> args2 = List.of();
             List<MoveDirection> results2 = OptionsParser.parse(args2);
             List<MoveDirection> expected2 = new ArrayList<>();
             Assertions.assertEquals(results2, expected2);
         });
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            String[] args3 = {"do tyłu", "fwd", "L"};
+            List<String> args3 = List.of("do tyłu", "fwd", "L");
             List<MoveDirection> results3 = OptionsParser.parse(args3);
             List<MoveDirection> expected3 = new ArrayList<>();
             Assertions.assertEquals(results3, expected3);
         });
 
         Assertions.assertDoesNotThrow(() -> {
-            String[] args4 = {"f", "b", "l", "r", "b", "r", "r", "l", "f"};
+            List<String> args4 = List.of("f", "b", "l", "r", "b", "r", "r", "l", "f");
             List<MoveDirection> results1 = OptionsParser.parse(args4);
             List<MoveDirection> expected1 = new ArrayList<>(Arrays.asList(MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.RIGHT, MoveDirection.BACKWARD, MoveDirection.RIGHT, MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.FORWARD));
             Assertions.assertEquals(results1, expected1);
