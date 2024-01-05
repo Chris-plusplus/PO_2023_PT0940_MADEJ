f2b3a89d0aae60a3d6f2859d3c9e56efb33ff068
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-01-05 10:21:05 +0100

    Adapted WorldElement for texture display

--- oolab/src/main/java/agh/ics/oop/model/WorldElement.java
+++ oolab/src/main/java/agh/ics/oop/model/WorldElement.java
@@ -1,6 +1,11 @@
 package agh.ics.oop.model;
 
+import javafx.scene.image.Image;
+
 public interface WorldElement {
     public Vector2d getPosition();
     public boolean isAt(Vector2d position);
+    public Image getTexture();
+    public String getLabel();
+    public WorldElementBox getBox();
 }
