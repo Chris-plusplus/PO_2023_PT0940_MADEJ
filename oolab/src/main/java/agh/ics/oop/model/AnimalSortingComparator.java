6baf603726d9504df05a9219d93ea340830af2fc
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-01-05 17:03:38 +0100

    made AnimalSortingComparator::compare() static and fixed object reference comparision

--- oolab/src/main/java/agh/ics/oop/model/AnimalSortingComparator.java
+++ oolab/src/main/java/agh/ics/oop/model/AnimalSortingComparator.java
@@ -2,14 +2,18 @@ package agh.ics.oop.model;
 
 import java.util.Comparator;
 
-public class AnimalSortingComparator implements Comparator<Animal> {
-    @Override
-    public int compare(Animal animal1, Animal animal2) {
-        if (animal1 == animal2){
-            return 0;
-        }
+public class AnimalSortingComparator {
+    public static int compare(Animal animal1, Animal animal2) {
         var pos1 = animal1.getPosition();
         var pos2 = animal2.getPosition();
-        return pos1.getX() < pos2.getX() || (pos1.getX() == pos2.getX() && pos1.getY() < pos2.getY()) ? -1 : 1;
+        if (pos1.getX() < pos2.getX() || (pos1.getX() == pos2.getX() && pos1.getY() < pos2.getY())){
+            return -1;
+        }
+        else if (pos1.equals(pos2)) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
     }
 }
