2509137d62954fcd35021c7ac7a4ad23e3804988
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-01-05 10:17:54 +0100

    Adapted Grass for texture display

--- oolab/src/main/java/agh/ics/oop/model/Grass.java
+++ oolab/src/main/java/agh/ics/oop/model/Grass.java
@@ -1,12 +1,23 @@
 package agh.ics.oop.model;
 
+import javafx.scene.image.Image;
+
 import java.util.Objects;
 
 public class Grass implements WorldElement {
     private final Vector2d position;
+    private WorldElementBox box;
+    private final static Image texture = new Image(Grass.class.getResourceAsStream("/textures/grass.png"));
 
     public Grass(Vector2d position){
         this.position = position;
+        try {
+            box = new WorldElementBox(this);
+        }
+        // JavaFX sie nie ładuje w testach
+        catch (NoClassDefFoundError | ExceptionInInitializerError ignored){
+            box = null;
+        }
     }
 
     public Vector2d getPosition() {
@@ -21,5 +32,20 @@ public class Grass implements WorldElement {
     @Override
     public String toString() {
         return "*";
+    }
+
+    @Override
+    public Image getTexture() {
+        return texture;
+    }
+
+    @Override
+    public String getLabel() {
+        return "Grass";
+    }
+
+    @Override
+    public WorldElementBox getBox() {
+        return box;
     }
 }
